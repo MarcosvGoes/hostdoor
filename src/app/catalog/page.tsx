3df7eb5d2e417215cd1f42bcb7d4eda6c2438504
Marcos <marcosviniciosgoes1@gmail.com>
2025-06-30 16:13:40 -0300

    update

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,7 +1,30 @@
-export default function PropertiesCatalog() {
+import Image from "next/image";
+
+async function getProperties() {
+  const res = await fetch("https://dommi-app.vercel.app/api/public/properties", {
+    next: { revalidate: 60 },
+  });
+
+  if (!res.ok) {
+    const errorText = await res.text(); // captura texto de erro
+    throw new Error(`Erro na API: ${res.status} - ${errorText}`);
+  }
+
+  return res.json();
+}
+
+
+export default async function PropertiesCatalog() {
+    const { properties } = await getProperties();
+
     return (
         <div>
-            Properties catalog
+            {properties.map((property: any) => (
+                <div key={property.id}>
+                    <h2>{property.title}</h2>
+                    <Image height={50} width={50} src={property.images[0]} alt="Imagem" />
+                </div>
+            ))}
         </div>
     )
 }
