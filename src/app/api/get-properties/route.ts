f46b42280ac7e9627dbb45d7b495a46d630e2349
Marcos <marcosviniciosgoes1@gmail.com>
2025-07-30 13:21:03 -0300

    update

--- src/app/api/get-properties/route.ts
+++ src/app/api/get-properties/route.ts
@@ -4,12 +4,11 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const baseUrl = process.env.NODE_ENV === "development" ? "http://localhost:3001" : AppDomain;
   console.log("Fetching from:", `${baseUrl}/api/properties`);
-  console.log("Using API_KEY:", process.env.API_KEY_LIST_PROPERTIES);
 
   const res = await fetch(`${baseUrl}/api/properties`, {
-    headers: {
-      Authorization: `Bearer ${process.env.API_KEY_LIST_PROPERTIES || ""}`,
-    },
+    // headers: {
+    //   Authorization: `Bearer ${process.env.API_KEY_LIST_PROPERTIES || ""}`,
+    // },
   });
 
   let data;
