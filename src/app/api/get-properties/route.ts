802a3e4ce03edade519c70dc619fee59a4a0b62c
Marcos <marcosviniciosgoes1@gmail.com>
2025-07-30 13:23:12 -0300

    update

--- src/app/api/get-properties/route.ts
+++ src/app/api/get-properties/route.ts
@@ -2,26 +2,22 @@ import { AppDomain } from "@/utils/routes";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const baseUrl = process.env.NODE_ENV === "development" ? "http://localhost:3001" : AppDomain;
-  console.log("Fetching from:", `${baseUrl}/api/properties`);
+  const baseUrl = process.env.NODE_ENV === "development"
+    ? "http://localhost:3001"
+    : AppDomain
 
   const res = await fetch(`${baseUrl}/api/properties`, {
     // headers: {
-    //   Authorization: `Bearer ${process.env.API_KEY_LIST_PROPERTIES || ""}`,
+    //   Authorization: `Bearer ${process.env.API_KEY_LIST_PROPERTIES}`,
     // },
   });
 
-  let data;
-  try {
-    data = await res.json();
-  } catch (err) {
-    console.error("Erro ao fazer parse do JSON:", err);
-    return NextResponse.json({ error: "Resposta inválida da API" }, { status: 500 });
-  }
+const data = await res.json();
 
-  if (!res.ok) {
-    return NextResponse.json(data, { status: res.status });
-  }
+if (!res.ok) {
+  return NextResponse.json(data, { status: res.status });
+}
 
-  return NextResponse.json(data);
+return NextResponse.json(data);
+
 }
