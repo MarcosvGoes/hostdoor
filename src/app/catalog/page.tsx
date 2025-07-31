e5503e3f516678704e1d265bbe83f875474cb5f1
Marcos <marcosviniciosgoes1@gmail.com>
2025-07-31 09:55:37 -0300

    update

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,6 +1,7 @@
 import { getPropertiesFromExternalAPI } from "@/features/properties/actions/getPropertiesFromExternalAPI";
 import PropertiesClientWrapper from "@/features/properties/components/PropertiesClientWrapper";
 
+export const dynamic = "force-dynamic";
 export const revalidate = 300;
 
 export default async function PropertiesCatalogPage() {
--- src/features/properties/actions/getPropertiesFromExternalAPI.ts
+++ src/features/properties/actions/getPropertiesFromExternalAPI.ts
@@ -1,17 +1,16 @@
 import { Property } from "@/shared/types/Property";
 
 export async function getPropertiesFromExternalAPI(): Promise<Property[]> {
-  const baseUrl = process.env.NODE_ENV === "development"
-    ? "http://localhost:3001"
-    : "https://dommi-rent.vercel.app";
-
-  const res = await fetch(`${baseUrl}/api/properties`, {
-    headers: {
-      Authorization: `Bearer ${process.env.API_KEY_LIST_PROPERTIES}`,
-    },
-    next: { revalidate: 300 },
-    cache: 'force-cache',
-  });
+  const res = await fetch(
+    `${process.env.NODE_ENV === "development" ? "http://localhost:3001" : "https://dommi-rent.vercel.app"}/api/properties`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.API_KEY_LIST_PROPERTIES}`,
+      },
+      next: { revalidate: 300 },
+      cache: "force-cache",
+    }
+  );
 
   if (!res.ok) {
     const errorText = await res.text();
