dbc56e50d051feb8c302fa72c70b8b26b29a10f2
Marcos <marcosviniciosgoes1@gmail.com>
2025-08-14 20:39:04 -0300

    update

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,4 +1,4 @@
 export const Domain = "https://hostdoor.com.br"
 export const AppDomain = "https://app.hostdoor.com.br"
 
-export const localhostDomain = "localhost:3001"
+export const localhostDomain = "localhost:3000"
