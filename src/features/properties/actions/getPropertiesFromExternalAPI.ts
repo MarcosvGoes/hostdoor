d9d2b1140518a3a48a46974280be4757c14d53d0
Marcos <marcosviniciosgoes1@gmail.com>
2025-08-22 18:25:25 -0300

    update

--- src/features/properties/actions/getPropertiesFromExternalAPI.ts
+++ src/features/properties/actions/getPropertiesFromExternalAPI.ts
@@ -4,11 +4,7 @@ import { appDomain, localhostAppDomain } from "@/utils/routes";
 
 export async function getPropertiesFromExternalAPI(): Promise<Property[]> {
   const res = await fetch(
-<<<<<<< HEAD
-    `${process.env.NODE_ENV === "development" ? localhostAppDomain : `${appDomain}`}/api/properties`,
-=======
-    `${process.env.NODE_ENV === "development" ? appDomain: `${localhostAppDomain }`}/api/properties`,
->>>>>>> develop
+    `${process.env.NODE_ENV === "development" ? `${appDomain}/api/properties` : `${localhostAppDomain }`}/api/properties`,
     {
       headers: {
         Authorization: `Bearer ${process.env.API_KEY_LIST_PROPERTIES}`,
--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,14 +1,5 @@
 export const domain = "https://hostdoor.com.br"
 export const appDomain = "https://app.hostdoor.com.br"
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-export const localhostDomain = "localhost:3000"
-=======
 export const localhostDomain = "http://localhost:3001"
 export const localhostAppDomain = "http://localhost:3000"
->>>>>>> refs/remotes/origin/main
-=======
-export const localhostDomain = "http://localhost:3001"
-export const localhostAppDomain = "http://localhost:3000"
->>>>>>> develop
