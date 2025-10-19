3bd79d787696639d9c627e03625837e4f012face
Marcos <marcosviniciosgoes1@gmail.com>
2025-10-18 22:19:25 -0300

    update

--- src/utils/routes.ts
+++
@@ -1,6 +0,0 @@
-export const domain = "https://hostdoor.com.br"
-export const appDomain = "https://app.hostdoor.com.br"
-export const devDomain = "https://app-hostdoor.vercel.app"
-
-export const localhostDomain = "http://localhost:3000"
-export const localhostAppDomain = "http://localhost:3001"
